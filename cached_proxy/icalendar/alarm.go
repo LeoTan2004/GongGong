d02d3aef5be46047b54eae3f8687b09ed7d3aad0
LeoTan2004 <3509836544@qq.com>
2025-02-02 22:51:04 +0800

    feat(ics): 添加通用日历编码
    - 添加icalendar规范下的日历文件编码
    
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

---
+++ cached_proxy/icalendar/alarm.go
@@ -0,0 +1,38 @@
+package icalendar
+
+import (
+	"strings"
+	"time"
+)
+
+type IcsAlarm struct {
+	action      string
+	trigger     time.Duration
+	description string
+}
+
+func (a *IcsAlarm) ToIcs(_ *Timezone) string {
+	result := strings.Builder{}
+	result.WriteString("BEGIN:VALARM\n")
+	if a.action != "" {
+		result.WriteString("ACTION:" + a.action + "\n")
+	} else {
+		result.WriteString("ACTION:DISPLAY\n")
+	}
+	result.WriteString("TRIGGER:" + DurationToIcs(a.trigger) + "\n")
+	result.WriteString("DESCRIPTION:" + a.description + "\n")
+	result.WriteString("END:VALARM\n")
+	return result.String()
+}
+
+func (a *IcsAlarm) SetAction(action string) {
+	a.action = action
+}
+
+func (a *IcsAlarm) SetTrigger(trigger time.Duration) {
+	a.trigger = trigger
+}
+
+func (a *IcsAlarm) SetDescription(description string) {
+	a.description = description
+}
